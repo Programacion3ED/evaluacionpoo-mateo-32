c957d4cff64efffadf5c82a37d3eba1d53cdd53f
mateo-32 <mateo.garzon.beltran@udla.edu.ec>
2026-05-21 15:39:46 -0500

    quinto envio Mateo Garzon

---
+++ src/main/java/ec/edu/poo/GestorActivos.java
@@ -0,0 +1,97 @@
+package ec.edu.poo;
+
+public class GestorActivos {
+
+    private ActivoDigital[] activos;
+    private int contador;
+
+    public GestorActivos() {
+        activos = new ActivoDigital[10];
+        contador = 0;
+    }
+
+    public boolean registrarActivo(ActivoDigital activo) {
+
+        
+        if (contador >= activos.length) {
+            return false;
+        }
+
+
+        for (int i = 0; i < contador; i++) {
+            if (activos[i].getCodigo().equals(activo.getCodigo())) {
+                return false;
+            }
+        }
+
+        activos[contador] = activo;
+        contador++;
+
+        return true;
+    }
+
+    public ActivoDigital buscarPorCodigo(String codigo) {
+
+        for (int i = 0; i < contador; i++) {
+            if (activos[i].getCodigo().equals(codigo)) {
+                return activos[i];
+            }
+        }
+
+        return null;
+    }
+
+    public int contarActivosCriticos() {
+
+        int cantidad = 0;
+
+        for (int i = 0; i < contador; i++) {
+            if (activos[i].getNivelRiesgo() >= 8) {
+                cantidad++;
+            }
+        }
+
+        return cantidad;
+    }
+
+    public double calcularPromedioRiesgo() {
+
+        if (contador == 0) {
+            return 0;
+        }
+
+        int suma = 0;
+
+        for (int i = 0; i < contador; i++) {
+            suma += activos[i].getNivelRiesgo();
+        }
+
+        return (double) suma / contador;
+    }
+
+    public boolean aplicarParcheActivo(String codigo) {
+
+        ActivoDigital activo = buscarPorCodigo(codigo);
+
+        if (activo != null) {
+            activo.setParcheAplicado(true);
+            return true;
+        }
+
+        return false;
+    }
+
+    public int obtenerCantidadActivos() {
+        return contador;
+    }
+
+    public ActivoDigital[] obtenerActivos() {
+        return activos;
+    }
+
+    public void reiniciar() {
+
+        activos = new ActivoDigital[10];
+        contador = 0;
+    }
+}
